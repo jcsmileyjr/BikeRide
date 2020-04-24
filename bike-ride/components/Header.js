371f14f089ca66712919cd328191f80cdf1bfefe
JC Smiley <jcsmileyjr@gmail.com>
2020-04-24 18:59:04 -0500

    Feature Home screen, set initial component placement
    - Install Native-base UI Library for easy layout building
    - Fix bug of missing file in node-modules by down-grading native-base.

---
+++ bike-ride/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View, StyleSheet } from 'react-native';
+import {Header, Text } from 'native-base';
+
+const Head = (props) => {
+	return (
+		<View>
+			<Header style={styles.headerContainer}>
+				<Text style={styles.textStyle}>Head</Text>
+			</Header>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	headerContainer: {
+		backgroundColor: "navy",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop:23,/*Bug where status bar covers the header. This push the header below the status bar */
+	},
+	textStyle:{
+		color:"white",
+	}
+});
+
+export default Head;
--- bike-ride/screens/home/Home.js
+++ bike-ride/screens/home/Home.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import {View, Text} from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Container, Footer,Text, Icon, H1, H2, H3 } from 'native-base';
 
-const Home =() => {
-	return(
-		<View>
-			<Text>Hello World</Text>
-			<Text>Hello World</Text>
-			<Text>Hello World</Text>
-			<Text>Hello World</Text>
-			<Text>Hello World</Text>
-		</View>
+import Header from '../../components/Header.js';
+
+const Home = () => {
+	return (
+		<Container>
+			<Header />
+			<View style={styles.contentlayout}>
+				<Icon style={styles.contentStyle} active name="md-sunny" />
+				<H1 style={styles.contentStyle} >Hero</H1>
+				<Text style={styles.contentStyle}>The temperature is 75 degrees</Text>
+				<Text style={styles.contentStyle}>Button</Text>
+			</View>
+			<Footer>
+				<Text>save button</Text>
+				<Text>Ride Criteria button</Text>
+			</Footer>
+		</Container>
 	);
 }
+
+const styles = StyleSheet.create({
+contentlayout:{
+	display:"flex",
+	flex:1,/*Use to take up all available space */
+},
+contentStyle:{
+	flex:1,	
+	textAlign:"center",
+},
+textStyle:{
+	flex:1,
+	textAlign:"center"
+},
+});
 
 export default Home;
