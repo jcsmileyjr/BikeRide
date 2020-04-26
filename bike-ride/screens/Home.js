31ab97c9c23a3465ff48b069173cbb74c355927b
JC Smiley <jcsmileyjr@gmail.com>
2020-04-26 10:31:55 -0500

    Feature: Show good or bad day image/message based on temperature
    - Created JSON with sample temperatures

--- bike-ride/screens/Home.js
+++ bike-ride/screens/Home.js
@@ -3,17 +3,40 @@ import { View, StyleSheet } from 'react-native';
 import { Container, Footer,Text, Icon, H1} from 'native-base';
 
 import Header from '../components/Header.js';
-import Button from '../components/Button.js';//Navigation button to the Forcast Screen
+import Button from '../components/Button.js';//Navigation button to the Forecast Screen
+import weather from '../data/weather.json';
 
 //First screen shown in the app that loads data for the app's operations
 const Home = ({navigation}) => {
+
+
+	//function that return true or false based on a criteria like temperature
+	getTodayForecast = () => {
+		const forecast = weather.weather[0];
+		if(forecast.temperature >= 60 && forecast.temperature <=85){
+			return true;
+		}
+		return false;
+	}
+
 	return (
 		<Container>
 			<Header title="Today" />
 			<View style={styles.contentlayout}>
-				<Icon style={[styles.mainImageStyle, styles.sunImage]} active name="md-sunny" />
-				<H1 style={styles.contentStyle} >Good Day to Ride</H1>
-				<Text style={styles.contentStyle}>The temperature is 75 degrees</Text>
+				{this.getTodayForecast() &&
+					<View style={styles.mainImageContainer}>
+						<Icon style={[styles.mainImageStyle, styles.sunImage]} name="md-sunny" />
+						<H1 style={styles.contentStyle} >Good Day to Ride</H1>
+					</View>
+				}
+				{!this.getTodayForecast() &&
+					<View style={styles.mainImageContainer}>
+						<Icon style={[styles.mainImageStyle, styles.stopHandImage]} type="FontAwesome" name="hand-stop-o" />
+						<H1 style={styles.contentStyle} >Do Not to Ride</H1>
+					</View>					
+				}								
+				
+				<Text style={styles.contentStyle}>The temperature is {weather.weather[0].temperature} degrees</Text>
 				<Button nav="Forecast" navigation={navigation} text="7 Day Forecast" />
 			</View>
 			<Footer style={styles.footerStyle}>
@@ -35,15 +58,21 @@ contentStyle:{
 	flex:1,	/*Evenly distribute space for each component in the content-layout section*/
 	textAlign:"center",
 },
+mainImageContainer:{
+	flex:2,/*Override to double area */
+},
 mainImageStyle:{/*Styles for the main two images at the top of the screen */
 	marginTop:20,/*white-space between Header and Primary Image */
 	marginBottom:40,/*white-space between Primary Image and other elements */
 	fontSize:250,/*Size of main image */
 	textAlign:"center",
-	flex:2,/*Override to double area */
+	
 },
 sunImage:{/*color of the sun image */
 	color:"yellow",
+},
+stopHandImage:{
+	color:"red",
 },
 footerStyle:{/*Style for the footer*/
 	paddingTop: 5,/*add white space above content */
