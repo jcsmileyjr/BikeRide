023ef419f896764e2aae47461f5539c638d42d12
Smiley <jcsmileyjr@gmail.com>
2020-05-02 11:32:27 -0500

    Refactor Home screen to increase size of content text based on feedback

--- bike-ride/screens/Home.js
+++ bike-ride/screens/Home.js
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
 	contentStyle: {
 		flex: 1,	/*Evenly distribute space for each component in the content-layout section*/
 		textAlign: "center",
+		fontSize:20,
 	},
 	mainImageContainer: {
 		flex: 2,/*Override to double area */
