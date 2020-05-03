94214fdbbfea1aaa0abf5652564f9fefe65f3da7
Smiley <jcsmileyjr@gmail.com>
2020-05-03 09:58:15 -0500

    Remove CSS from Edit Criteria and Documentation to Button Component

--- bike-ride/screens/SetCriteria.js
+++ bike-ride/screens/SetCriteria.js
@@ -66,22 +66,6 @@ const styles = StyleSheet.create({
 		display: "flex",
 		flex: 1,
 	},
-    textInputStyle:{/*Style for input fields */
-		width: 270,
-		color: 'grey',  //blue text color
-		textAlign: "center",
-		height: 40,
-		borderColor: "grey",
-		borderWidth: 1,
-		fontSize: 18,
-    },
-    boldLabel:{
-        fontWeight:"bold",
-    },
-    inputLabelStyle:{/*Style for labels */
-        textAlign:'center',/*center the label text*/
-        color:"grey",/*add color */
-    },
     inputContainer:{
         display:"flex",/* center the content */
         justifyContent:"center",/* center the content */
