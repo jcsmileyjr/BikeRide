089071170523b5c293cad58e8af361321a498024
Smiley <jcsmileyjr@gmail.com>
2020-05-04 11:01:04 -0500

    Update SavePredictions component's documentation

--- bike-ride/components/SavePredictions.js
+++ bike-ride/components/SavePredictions.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View, Text, StyleSheet, AsyncStorage} from 'react-native';
-import {Icon, Toast} from 'native-base';/*UI library for styling and complex components missing from the React Native */
+import {Icon, Toast} from 'native-base';// UI library for styling and complex components missing from the React Native
 
+// Icon and text in the footer on the Forecast screen to save the current 7 day forecast to be use later if there is no internet access
 const SavePredictions = (props) => {
 
     saveData = async () => {
-		await AsyncStorage.setItem("predictions",JSON.stringify(props.forecast));//Save array of weather objects to local storage 
+		await AsyncStorage.setItem("predictions",JSON.stringify(props.forecast));// Save array of weather objects to local storage 
 		Toast.show({text:"7 Day Forecast has been saved!!!", position:"bottom", type:"success", textStyle:{color:"white", textAlign:"center"}, duration:3000});
     }
 
@@ -18,9 +19,9 @@ const SavePredictions = (props) => {
 }
 
 const styles = StyleSheet.create({
-	cogIconStyle:{/*Style for the save icon in the footer */
-		color:"darkgreen",/*Color of the icon*/
-		textAlign:"center",/*Center the icon in its row*/
+	cogIconStyle:{
+		color:"darkgreen",
+		textAlign:"center",
 	},
 	footerTextStyle:{
 		color:"black",
