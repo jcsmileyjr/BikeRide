dbf1256ae21485a5f1ae1b9a2d4a1839cf4e618a
Smiley <jcsmileyjr@gmail.com>
2020-05-08 12:13:51 -0500

    Add unit test for converting Fahrenheit to Celsius

--- bike-ride/__tests__/home.test.js
+++ bike-ride/__tests__/home.test.js
@@ -1,6 +1,22 @@
 import {sanitizeCurrentWeatherData} from '../js/sanitizeData.js';
 import {applyRidingCriteria} from '../js/applyRideCriteria.js';
+import {convertToCelsius} from '../js/homeScreenFunctions.js';
 import baseRideCriteria from '../js/baseRideCriteria.js';
+
+const goodForecast = {
+    "temperature":80,
+    "windSpeed":12,
+    "ifRained":0,
+    "date":"05/03/2020"
+}
+
+const badForecast = {
+    "temperature":50,
+    "windSpeed":26,
+    "ifRained":2,
+    "date":"05/06/2020"
+}
+
 describe("IF weather data from an API call is ready to be displayed?", () => {
     const testAPIData = {
         "current":{
@@ -10,26 +26,17 @@ describe("IF weather data from an API call is ready to be displayed?", () => {
         },
         "location":"03/05/2020"
     }
+
     it('should convert the unknown data into a useable object and return the correct temperature', () => {
         expect((sanitizeCurrentWeatherData(testAPIData)).temperature).toBe(80);
+    });
+
+    it('should convert convert Fahrenheit to Celsius', () => {
+        expect(convertToCelsius(goodForecast.temperature)).toBe(27);
     });
 });
 
 describe("If the riding criteria is applied successfully to a weather object?", () => {
-    const goodForecast = {
-        "temperature":80,
-        "windSpeed":12,
-        "ifRained":0,
-        "date":"05/03/2020"
-    }
-
-    const badForecast = {
-        "temperature":50,
-        "windSpeed":26,
-        "ifRained":2,
-        "date":"05/06/2020"
-    }
-
     it("should return true if the object passes the criteria", () => {
         expect(applyRidingCriteria(goodForecast, baseRideCriteria)).toBeTruthy();
     });
